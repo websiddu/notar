0e6c38aba08a1ab835cc93c77cd332ef1c8f382e
Siddhartha Gudipati <me@websiddu.com>
2016-07-05 01:23:33 -0700

    Adding auth guard

--- src/app/+doc/doc.routes.ts
+++ src/app/+doc/doc.routes.ts
@@ -1,7 +1,10 @@
 import { RouterConfig } from '@angular/router';
 import { DocComponent } from './doc.component';
 
+import { AuthGuard } from '../shared/services/auth/auth.guard.service';
+import { CanDeactivateGuard } from '../shared/services/auth/auth.interfaces';
+
 export const DocRoutes: RouterConfig = [
-  { path: 'doc', component: DocComponent },
-  { path: 'doc/:id', component: DocComponent }
+  { path: 'doc', component: DocComponent, canActivate: [AuthGuard] },
+  { path: 'doc/:id', component: DocComponent, canDeactivate: [CanDeactivateGuard]}
 ];
--- src/app/+login/login.routes.ts
+++ src/app/+login/login.routes.ts
@@ -1,40 +1,16 @@
-import { RouterConfig }          from '@angular/router';
-// import { CrisisDetailComponent } from './crisis-detail.component';
-// import { CrisisListComponent }   from './crisis-list.component';
 import { AuthGuard } from '../shared/services/auth/auth.guard.service';
 import { AuthService } from '../shared/services/auth/auth.service';
+
 import { LoginComponent } from './login.component';
-// import { CrisisAdminComponent }  from './crisis-admin.component';
 
-// import { CanDeactivateGuard }    from '../interfaces';
-// import { AuthGuard }             from '../auth.guard';
 
-export const LoginRoutes: RouterConfig = [
+export const LoginRoutes = [
   {
     path: '',
     redirectTo: '/login',
     terminal: true
   },
-  {
-    path: 'login',
-    component: LoginComponent,
-    // children: [
-    //   {
-    //     path: 'admin',
-    //     component: CrisisAdminComponent,
-    //     canActivate: [AuthGuard]
-    //   },
-    //   {
-    //     path: ':id',
-    //     component: CrisisDetailComponent,
-    //     canDeactivate: [CanDeactivateGuard]
-    //   },
-    //   {
-    //     path: '',
-    //     component: CrisisListComponent
-    //   }
-    // ]
-  }
+  { path: 'login', component: LoginComponent }
 ];
 
 export const AUTH_PROVIDERS = [AuthGuard, AuthService];
