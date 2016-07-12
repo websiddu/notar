2c72b710823d439541ec1165ad6137021d518c63
Siddhartha Gudipati <me@websiddu.com>
2016-07-12 08:23:34 -0700

    Adding the doc html

--- src/app/+docs/shared/onboarding/onboarding.component.ts
+++ src/app/+docs/shared/onboarding/onboarding.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+
+
 import { ModalComponent } from '../../../shared/common/modal/modal.component';
 
 import { CarouselComponent } from '../../../shared/common/carousel/carousel.component';
 import { SlideComponent } from '../../../shared/common/carousel/slide/slide.component';
 
 import { ApiService } from '../../../shared/services/api/api.service';
+
+declare var firebase;
 
 @Component({
   moduleId: module.id,
@@ -20,15 +25,19 @@ export class OnboardingComponent implements OnInit {
   folderName: string;
   folderId: string;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private af: AngularFire) {}
 
   onSubmit() {
     if (!this.folderName) {
       return;
     }
     this.api.createFolder(this.folderName).then( (response) => {
-      this.folderId = response.body.id;
-      this.api.updateUserConfig();
+      this.folderId = response.result.id;
+      let uid = firebase.auth().currentUser.uid;
+      firebase.database().ref(`users/${uid}/folder`).set({
+          'id': this.folderId,
+          'name': this.folderName,
+        });
     });
   }
 
